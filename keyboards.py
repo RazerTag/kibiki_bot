29015ffdc2bf7f5d6789bf206dc2bff185f143fc
Gleb Sanin <gvsanin@edu.hse.ru>
2025-05-02 17:43:30 +0300

    fixed keyboards

--- keyboards.py
+++ keyboards.py
@@ -1,35 +1,22 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Меню для пользователя
-user_menu = ReplyKeyboardMarkup(resize_keyboard=True)
-user_menu.add(
-    KeyboardButton("📊 Мой баланс"),
-    KeyboardButton("📜 Моя история"),
-)
-user_menu.add(
-    KeyboardButton("📢 Объявления"),
+user_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton("📊 Мой баланс"), KeyboardButton("📜 Моя история")],
+        [KeyboardButton("📢 Объявления")],
+    ],
+    resize_keyboard=True
 )
 
 # Меню для администратора
-admin_menu = ReplyKeyboardMarkup(resize_keyboard=True)
-admin_menu.add(
-    KeyboardButton("➕ Начислить Кибики"),
-    KeyboardButton("➖ Списать Кибики"),
-)
-admin_menu.add(
-    KeyboardButton("🔍 Баланс студента"),
-    KeyboardButton("🗃 История студента"),
-)
-admin_menu.add(
-    KeyboardButton("🧑‍🎓 Добавить студента"),
-    KeyboardButton("❌ Удалить студента"),
-)
-admin_menu.add(
-    KeyboardButton("📢 Новое объявление"),
-    KeyboardButton("🧹 Удалить объявление"),
-)
-admin_menu.add(
-    KeyboardButton("📊 Мой баланс"),
-    KeyboardButton("📜 Моя история"),
-    KeyboardButton("📢 Объявления"),
+admin_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton("➕ Начислить Кибики"), KeyboardButton("➖ Списать Кибики")],
+        [KeyboardButton("🔍 Баланс студента"), KeyboardButton("🗃 История студента")],
+        [KeyboardButton("🧑‍🎓 Добавить студента"), KeyboardButton("❌ Удалить студента")],
+        [KeyboardButton("📢 Новое объявление"), KeyboardButton("🧹 Удалить объявление")],
+        [KeyboardButton("📊 Мой баланс"), KeyboardButton("📜 Моя история"), KeyboardButton("📢 Объявления")],
+    ],
+    resize_keyboard=True
 )
