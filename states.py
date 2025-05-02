8e94150689423d15dccc4c1d8fc301f52e79c7da
Gleb Sanin <gvsanin@edu.hse.ru>
2025-05-02 17:57:18 +0300

    fixed states

--- states.py
+++ states.py
@@ -1,7 +1,12 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class AddEventStates(StatesGroup):
+    """States for the /addevent FSM: name, date, location, and points steps."""
     name = State()
     date = State()
     location = State()
     points = State()
+
+class AnnounceStates(StatesGroup):
+    """States for the /announce FSM: waiting for announcement text."""
+    text = State()
